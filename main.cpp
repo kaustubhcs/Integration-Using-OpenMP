d5f6daf6d20ad8db29f4f8a12fc6d6223e1e82a8
Kaustubh Shivdikar <kaustubhcs07@gmail.com>
2017-10-19 00:18:21 -0400

    Functional Code Updated ~KTB

--- main.cpp
+++ main.cpp
@@ -4,16 +4,32 @@
 
 # include <iostream>
 # include <omp.h>
+# include <iomanip>
+
+# define M_PI 3.14159265358979323846
+
+using namespace std;
+
 
 int main() {
 
     long steps = 1000;
+    double answer = 0;
 
-#pragma omp parallel
-    {
-
-
+#pragma omp parallel for
+    for (long i = 0; i < steps; i++) {
+    answer += (4 / 1 + (i*i));
     }
+
+    cout.precision(19);
+    cout << "Calculated PI: "<< answer << endl;
+    cout << "Math PI: " << M_PI << endl;
+    double difference;
+    difference = answer - M_PI;
+    cout << "Difference is: " << difference << endl;
+
+
+
 
     return 0;
 }
