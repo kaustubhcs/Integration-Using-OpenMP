4283a9d740a349639abf074352f5155c23df48e3
Kaustubh Shivdikar <kaustubhcs07@gmail.com>
2017-10-18 22:08:54 -0400

    Code Initiated
    ~KTB

---
+++ cmake-build-debug/CMakeFiles/Integration_Using_OpenMP.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Integration_Using_OpenMP.dir/main.cpp.o"
+  "Integration_Using_OpenMP.pdb"
+  "Integration_Using_OpenMP"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Integration_Using_OpenMP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Kaustubh Shivdikar on 10/18/17.
+//
+
+# include <iostream>
+# include <omp.h>
+
+int main() {
+
+    long steps = 1000;
+
+#pragma omp parallel
+    {
+
+
+    }
+
+    return 0;
+}
