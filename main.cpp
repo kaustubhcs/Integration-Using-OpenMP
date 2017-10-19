929f5a819180436caff9ebf35c354ca26bea9ba2
Kaustubh Shivdikar <kaustubhcs07@gmail.com>
2017-10-19 00:38:42 -0400

    Functional Code Updated ~KTB

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ int main() {
     omp_set_num_threads(1);
 
 #pragma omp parallel for
-    for (int i = 0; i < 1; i++) {
+    for (int i = 0; i < steps; i++) {
         double loop_i = i/1000;
 
         answer += (4.0 / 1 + (loop_i * loop_i) * ((1.0 / steps)));
